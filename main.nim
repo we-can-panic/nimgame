b4be33e78e3cd9f350673016fec6fae6cc1ccdc7
we-can-panic <koshikazuma@gmail.com>
2022-03-13 19:08:40 +0900

    add wavelength join api

--- main.nim
+++ main.nim
@@ -1,42 +1,26 @@
 #[
   main
     - rounting
-    - logging
 ]#
-import jester, posix, json, logging, os, strutils, asyncdispatch
-import htmlgen as h
+import os, asyncdispatch, strutils
+import jester
+
+import general/logutils
+import wavelength/back/back as wl
 
 
-# logging
-
-let fl = newFileLogger("logs.log", fmtStr="$datetime $levelname ")
-
-proc log_debug(args: varargs[string, `$`]) =
-  debug args
-  fl.file.flushFile()
-
-proc log_info(args: varargs[string, `$`]) =
-  info args
-  fl.file.flushFile()
-
-onSignal(SIGABRT):
-  echo "<2>Received SIGABRT"
-  quit(1)
-
-fl.addHandler
-
-
-# rounting
-
-var settings = newSettings()
+let settings = newSettings()
 if existsEnv("PORT"):
   settings.port = Port(parseInt(getEnv("PORT")))
 
 routes:
   get "/":
-    resp h.h1("Hello myservice!!!")
+    resp "<h1>Hello myservice!!!</h1>"
   get "/wavelength":
     resp "<html><head></head><body>hello</body></html>"
+  get "/wavelength/ws":
+    await wl.onRequest(request)
+    resp "WebSocket Only"
 
 when isMainModule:
   log_info "starting"
---
+++ wavelength/back/back.nim
@@ -0,0 +1,122 @@
+#[
+  WebSocketの処理
+]#
+
+import json, strutils, sequtils
+import jester, ws, ws/jester_extra
+
+import ../../general/logutils
+import ../core/core
+
+type
+  # 接続中のユーザー
+  WSUser = ref object of User
+    key: string
+    conn: WebSocket
+
+var currentUsers {.threadvar.} : seq[WSUser]
+## connecting users
+
+proc newUser(ws: WebSocket): WSUser
+## create new user from websocket
+proc isExists(user: WSUser): bool
+## judge `currentUsers` is including this user
+proc regist(user: WSUser)
+## join user to `currentUsers`
+proc exportUsers(): seq[JsonNode]
+## List current users for Api `Users`
+proc send(user: WSUser, msg: string)
+## send query to user
+proc send(users: seq[WSUser], msg: string)
+## send query to some users
+proc sendAll(msg: string)
+## send query to all users
+
+
+proc onRequest* (request: Request) {.async.} =
+  try:
+    var ws = await newWebSocket(request)
+    var user = newUser(ws)
+    while ws.readyState == Open:
+      var
+        msg = await ws.receiveStrPacket()
+        (res, query) = parsePacket(msg)
+
+      if not res:
+        logInfo "jsonparseError"
+        continue
+
+      if not query.hasKey("type"):
+        loginfo "Invalid api format: " & $query
+        continue
+
+      var apiType = parseEnum[ApiSend](query["type"].getStr)
+      case apiType:
+      of Join:
+        user.name = query["name"].getStr
+        if not user.isExists:
+          regist(user)
+        let
+          userlist = exportUsers()
+          usersquery = %* {
+            "type": "",
+            "users": userlist
+          }
+        sendAll($usersquery)
+
+      of Status:
+        discard
+      of Dial1:
+        discard
+      of Dialed:
+        discard
+
+  except JsonParsingError:
+    logDebug "Json invalid: " & getCurrentExceptionMsg()
+  except WebSocketClosedError:
+    logInfo "Socket closed."
+  except WebSocketProtocolMismatchError:
+    logError "Socket tried to use an unknown protocol: ", getCurrentExceptionMsg()
+  except WebSocketError:
+    logError "Unexpected socket error: ", getCurrentExceptionMsg()
+
+
+
+
+proc newUser(ws: WebSocket): WSUser =
+  ## create new user from websocket
+  result = WSUser()
+  result.key = ws.key
+  result.conn = ws
+
+proc isExists(user: WSUser): bool =
+  ## judge `currentUsers` is including this user
+  result = currentUsers.mapIt(it.key).contains(user.key)
+
+proc regist(user: WSUser) =
+  ## join user to `currentUsers`
+  currentUsers.add(user)
+
+proc exportUsers(): seq[JsonNode] =
+  ## List current users for Api `Users`
+  for user in currentUsers:
+    if user.conn.readyState == Open:
+      result.add( %* {
+        "name": user.name,
+        "status": $user.status
+      })
+
+proc send(user: WSUser, msg: string) =
+  ## send query to user
+  let conn = user.conn
+  if conn.readyState == Open:
+    asyncCheck conn.send(msg)
+
+proc send(users: seq[WSUser], msg: string) =
+  ## send query to some users
+  for usr in users:
+    usr.send(msg)
+
+proc sendAll(msg: string) =
+  ## semd query tp all users
+  send(currentUsers, msg)

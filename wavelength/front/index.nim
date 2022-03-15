bf0a7dd2019c6a4159e6e8e97ac30ec46734906e
we-can-panic <koshikazuma@gmail.com>
2022-03-15 21:44:52 +0900

    add front on test

--- wavelength/front/index.nim
+++ wavelength/front/index.nim
@@ -2,9 +2,11 @@
   描画/通信
 ]#
 
-import json, jsffi
+import json, jsffi, strutils
 include karax/prelude
+import karax / [kdom, vdom, karax, karaxdsl, jstrutils, compact, localstorage, vstyles]
 import karax/jwebsockets
+import ../core/core
 
 # WebSocket Part #
 const
@@ -13,16 +15,49 @@ const
     when test: "ws://localhost:5000/wavelength/ws"
     else:      "ws://khc-nimgame.herokuapp.cpm/wavelength/ws"
 
-var ws = newWebSocket(url)
+var
+  user = new User
+  ws = newWebSocket(url)
+  otherUsers: seq[User]
 
 ws.onmessage = proc(ev: MessageEvent) =
-  echo "receive: ", ev.data
+  var (res, query) = parsePacket($ev.data)
+  if not res:
+    echo "jsonparseError"
+    return
+  if not query.hasKey("type"):
+    echo "Invalid api format: " & $query
+    return
+  echo "got: "
+  echo query
 
-proc send() =
-  var query = %* {"type": "Join", "name": "ffff"}
-  ws.send($query)
-  echo "send: " & $query
+  var apiType = parseEnum[ApiReceive](query["type"].getStr)
+  case apiType:
+  of Users:
+    discard
+  of Host:
+    discard
+  of Range1:
+    discard
+  of Theme:
+    discard
+  of Dial2:
+    discard
+  of Score:
+    discard
+  of End:
+    discard
 
+proc send(apitype: ApiSend, query: JsonNode) =
+  var typedQuery = query
+  typedQuery["type"] = %* $apitype
+  ws.send($typedQuery)
+  echo "send: " & $typedQuery
+
+proc sendJoin() =
+  ## send request Join
+  var name = $getElementById("login-input").value
+  send(Join, %* {"name": name})
 
 # HTML Part #
 proc createHtml* (): VNode =
@@ -32,9 +67,21 @@ proc createHtml* (): VNode =
       title:
         text "NIMGAME"
     body:
-      h1:
-        text "Helllllllo"
-        proc onclick(ev: Event, n: VNode) =
-          send()
+      case user.room:
+      of Login:
+        tdiv():
+          h1: text "wavelength"
+          h2: text "Login"
+          input(`type`="text", id="login-input")
+          p()
+          button:
+            text "Enter"
+            proc onclick(ev: Event, n: VNode) =
+              sendJoin()
+      of Wait:
+        discard
+      of Game:
+        discard
+
 
 setRenderer createHtml
